c5aa1debca995c9852e12f7c8ff6059db6a31a7e
AnjalaD <anjaladilhara@gmail.com>
2019-10-11 09:45:38 +0530

    init + basic componnet files

---
+++ src/components/content.js
@@ -0,0 +1,64 @@
+export const content = {
+    header: {
+        title: "Page Title",
+        logo: {
+            src: "",
+            alt: ""
+        },
+        nav: [
+            {
+                name: "Home",
+                link: "/"
+            },
+            {
+                name: "Contact",
+                link: "/contact"
+            }
+        ]
+    },
+    body: {
+        slogon: "",
+        slide_show: [
+            {
+                src: "",
+                alt: ""
+            },
+            {
+                src: "",
+                alt: ""
+            }
+        ],
+        items: [
+            {
+                name: "",
+                price: "",
+                desc: "",
+                img: {
+                    src: "",
+                    alt: ""
+                }
+            },
+            {
+                name: "",
+                price: "",
+                desc: "",
+                img: {
+                    src: "",
+                    alt: ""
+                }
+            },
+        ],
+    },
+    footer: {
+        social_media: {
+            facebook: {
+                active: true,
+                link: ""
+            },
+            instagram: {
+                active: false,
+                link: ""
+            }
+        }
+    }
+};
